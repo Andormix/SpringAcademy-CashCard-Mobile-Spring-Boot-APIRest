63c42acd091c91cde0a6023eaf8c922560f902d8
Eric Torrontera Ruiz <eric@aNDORRA.AD>
2026-09-17 19:13:13 +0200

    feat(config): bootstrapped Spring Security

--- src/main/java/com/andormix/cashcard/SecurityConfig.java
+++ src/main/java/com/andormix/cashcard/SecurityConfig.java
@@ -1,25 +1,31 @@
 package com.andormix.cashcard;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+// The @Configuration annotation tells Spring to use this class to configure Spring and Spring Boot itself.
+// Any Beans specified in this class will now be available to Spring's Auto Configuration engine.
+@Configuration
 class SecurityConfig {
-//
-//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http.build();
-//    }
-//
-//    @Bean
-//    PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 }
